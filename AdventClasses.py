6021aaad19f69f9c42466136c1ce5b5675d2ede1
Johannes Betz <betz@campus.tu-berlin.de>
2019-12-24 13:56:46 +0100

    added comments to intcomputer class and advent5 main file

--- AdventClasses.py
+++ AdventClasses.py
@@ -25,6 +25,7 @@ class IntComputer:
         self.input            = queue.LifoQueue()
         self.addressing_modes  = [0,0,0]                 
 
+    # The string of instructions is parsed as a list of ints into the RAM of the Int Computer
     def parse_instruction(self, instruction_string):
 
         self.memory = list(map(lambda x: int(x), instruction_string.split(",")))
@@ -33,8 +34,7 @@ class IntComputer:
 
         self.program_pointer += 1
 
-    
-
+    """ Instruction functions of the int computer """
     # Instruction 1: Addition
     def add(self):
 
@@ -147,7 +147,14 @@ class IntComputer:
             # later adcents day
             print("bla")
     
-    # 
+    # The opcode (current adress the pointer points on) is filled up to NUM_ADDRESSING_MODES + INSTRUCTION_LEN (=5) digits in total
+    # The first left NUM_ADDRESSING_MODES (=3) of the opcode determine the mode of adressing the target of the operation, followed by INSTRUCTION_LEN(2) instructions
+    # Let (AM3, AM2, AM1, I2, I1) be the zero padded opcode and, then the entries mean:
+    # AM3 = adressing mode of the target 
+    # AM2 = adressing mode of the second argument
+    # AM1 = adressing mode of the first argument
+    # I2, I1 = Instruction 
+    # TODO: current value in memory = instruction = adressing, opcode (the other way round)
     def build_adressing_modes(self, opcode):
 
         # Fill up opcode with zeros to constant length
@@ -155,6 +162,7 @@ class IntComputer:
 
         self.addressing_modes = list(map( lambda x : int(x), list(opcode_str[:self.NUM_ADDRESSING_MODES])))
 
+    # execution of the programm based on the values of the puzzle inputs
     def execute_programm(self):
         self.program_pointer =0
 
@@ -171,10 +179,11 @@ class IntComputer:
             # Evaluate the correct function 
             self.instruction_dict[instruction_code]()
 
-    
+    # Reads the memory of the int computer at a given address
     def read_memory(self, adress):
         return self.memory[adress]
 
+    # Sets the memory of the int computer at a given address 
     def set_memory(self, adress, value):
         self.memory[adress] = value
 
--- advent_05.py
+++ advent_05.py
@@ -1,13 +1,16 @@
-from IntComputer import *
+from AdventClasses import IntComputer
 
 f = open("advent5_input.txt", "r")
 puzzle_data = f.read()
 
+# Initiate an Int computer 
 intcomputer = IntComputer()
 
-# Set manually to a specific number (given in assigment)
+# Set input manually to a specific number (given in assigment)
 intcomputer.input.put(5)  
 
+# Parse the puzzle input in the RAM of the int computer
 intcomputer.parse_instruction(puzzle_data)
+
 
 intcomputer.execute_programm()
